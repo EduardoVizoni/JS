08cf621cbc699a979e6cd7de7f91e218822d10e5
Eduardo Vizoni do Prado <145796079+EduardoVizoni@users.noreply.github.com>
2024-09-09 21:58:59 -0300

    .
    .

--- Training/exe20.js
+++ Training/exe20.js
@@ -5,3 +5,21 @@ Adicione um método ao objeto carro chamado getIdade que retorne quantos anos o 
 tem (ano atual - ano do carro).
 Adicione um método ao objeto carro chamado getDescricao que retorne uma string
 contendo todas as informações do carro. */
+
+let carro = {
+    marca: "Mustang",
+    modelo: "Charger HellCat STR ",
+    anoCarro: 2023,
+
+    getIdade: function() {
+        const anoAtual = new Date().getFullYear();
+        return anoAtual - this.anoCarro;
+    },
+    
+    getDescricao: function() {
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`;
+    }
+};
+console.log(carro.marca);
+console.log(carro.getIdade());
+console.log(carro.getDescricao());
