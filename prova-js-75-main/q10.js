755e49b2cb7aef40864d3429840731146c9ac0c0
Eduardo Vizoni do Prado <145796079+EduardoVizoni@users.noreply.github.com>
2024-10-07 16:52:28 -0300

    .
    .

---
+++ prova-js-75-main/q10.js
@@ -0,0 +1,49 @@
+// q10.js
+/**
+ * Questão 10: Manipulação de Array de Palavras
+ *
+ * Implemente uma função chamada manipularArrayPalavras que recebe um array de strings como parâmetro.
+ * Esta função deve realizar uma série de operações no array e retornar um objeto com os resultados.
+ *
+ * Requisitos:
+ * a) Crie uma cópia do array original.
+ * b) Converta todas as palavras da cópia para letras maiúsculas.
+ * c) Adicione a palavra "INÍCIO" no início da cópia do array.
+ * d) Adicione a palavra "FIM" no final da cópia do array.
+ * e) Encontre a palavra mais longa na cópia do array.
+ * f) Crie um novo array apenas com as palavras que começam com a letra "A".
+ *
+ * Exemplo de uso:
+ * const palavras = ["casa", "arvore", "carro", "aviao"];
+ *   Exporte esses objetos:
+ *   return {
+ *   arrayOriginal: palavras,
+ *   arrayCopia: arrayCopia,
+ *   palavraMaisLonga: palavraMaisLonga,
+ *   palavrasComA: palavrasComA
+ * };
+ *
+ */
+/* function manipularArrayPalavras(palavras) {
+    const palavras = ["casa", "arvore", "carro", "aviao"];
+    console.log(palavras.toUpperCase());
+    palavras.unshift("INÍCIO");
+    palavras.push("FIM");
+    let copia = palavras.slice();
+    console.log(copia);
+    const palavasA = ["arvore", "aviao"]
+    // Converta todas as palavras da cópia para letras maiúsculas = eu sei que é utilizando to.UpperCase
+    // agora como, n sei
+
+    return {
+        arrayOriginal: palavras,
+        arrayCopia: arrayCopia,
+        palavraMaisLonga: palavraMaisLonga,
+        palavrasComA: palavrasComA
+    };
+}
+module.exports = manipularArrayPalavras;
+*/
+
+// vou ter que deixar essa questão em comentários, porque faltando 10 minutos ela começou
+// a dar erro e eu não entendi o porque
---
+++ prova-js-75-main/q7.js
@@ -0,0 +1,26 @@
+// q7.js
+/**
+ * Questão 7: Unir e remover duplicados em dois arrays
+ *
+ * Implemente uma função chamada unirArrays que receba dois arrays como parâmetros
+ * e retorne um novo array contendo todos os elementos dos dois arrays, sem elementos duplicados.
+ *
+ * Requisitos:
+ * 1. O array resultante deve conter apenas elementos únicos (sem duplicatas).
+ * 2. A ordem dos elementos deve seguir a ordem de aparecimento nos arrays originais.
+ *
+ * Exemplo de uso:
+ * const resultado = unirArrays([1, 2, 3, 4], [3, 4, 5, 6]);
+ * console.log(resultado); // Deve imprimir: [1, 2, 3, 4, 5, 6]
+ */
+function unirArrays(array1, array2) {
+    let resultado = array1.concat(array2);
+    let unicos = [];
+    for (let item of resultado) {
+        if (!unicos.includes(item)) {
+            unicos.push(item);
+        }
+    }
+    return unicos;
+}
+module.exports = unirArrays;
